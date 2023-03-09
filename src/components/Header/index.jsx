77f696b1785f468f6be21ef5b44992694dad6bdf
Ayush Tyagi <ak7046243581@gmail.com>
2023-03-10 01:00:36 +0530

    feat: add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+export default function LandingPage() {
+    return (
+        <div className='header'>
+            <div className='header-title'>
+                <span>CMS+</span>
+            </div>
+            <div className='content-type'>
+                <span>Content Types</span>
+            </div>
+        </div>
+    );
+}
