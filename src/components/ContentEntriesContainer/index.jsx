db0aa31432c964a04eff3916ebd30717a85f76a4
Ayush Tyagi <ak7046243581@gmail.com>
2023-03-10 03:15:18 +0530

    feat: add content-entries-container, modal and new-entry-modal components

---
+++ src/components/ContentEntriesContainer/index.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import editIcon from '../../assets/user-edit-text-message-note@3x.png';
+import binIcon from '../../assets/trash-delete-recycle-bin-bucket-waste@3x.png';
+import './ContentEntriesContainer.css';
+import NewEntryModal from '../NewEntryModal';
+
+export default function ContentEntriesContainer() {
+    const [showModal, setShowModal] = React.useState(false);
+    return (
+        <div>
+            <div className='content-entry-container'>
+                <div className='content-entrie-title'>
+                    <span className='entrie-count'>13 Entries Found</span>
+                    <div onClick={() => setShowModal(true)} className='new-entry-div'>
+                        <span className='new-entry-text'>Add a new entry</span>
+                    </div>
+                    <NewEntryModal onClose={() => setShowModal(false)} show={showModal} />
+                </div>
+                <div className='table-content-title'>
+                    <div className='content-entries-fields'>
+                        <span>Id</span>
+                        <span>Name</span>
+                        <span>Website</span>
+                        <span>Contact</span>
+                    </div>
+                    <span>Actions</span>
+                </div>
+                <div className='content-entries'>
+                    <div className='content-fields-container'>
+                        <div className='content-entries-fields' style={{ marginLeft: '10px' }}>
+                            <span>1</span>
+                            <span>John Doe</span>
+                            <span>www.johndoe.com</span>
+                            <span>Text</span>
+                        </div>
+                        <div>
+                            <img className='icon' src={editIcon} alt="" />
+                            <img className='icon' src={binIcon} alt="" />
+                        </div>
+                    </div>
+                    <div className='content-fields-container'>
+                        <div className='content-entries-fields' style={{ marginLeft: '10px' }}>
+                            <span>1</span>
+                            <span>John Doe</span>
+                            <span>www.johndoe.com</span>
+                            <span>Text</span>
+                        </div>
+                        <div>
+                            <img className='icon' src={editIcon} alt="" />
+                            <img className='icon' src={binIcon} alt="" />
+                        </div>
+                    </div>
+                    <div className='content-fields-container'>
+                        <div className='content-entries-fields' style={{ marginLeft: '10px' }}>
+                            <span>1</span>
+                            <span>John Doe</span>
+                            <span>www.johndoe.com</span>
+                            <span>Text</span>
+                        </div>
+                        <div>
+                            <img className='icon' src={editIcon} alt="" />
+                            <img className='icon' src={binIcon} alt="" />
+                        </div>
+                    </div>
+                    <div className='content-fields-container'>
+                        <div className='content-entries-fields' style={{ marginLeft: '10px' }}>
+                            <span>1</span>
+                            <span>John Doe</span>
+                            <span>www.johndoe.com</span>
+                            <span>Text</span>
+                        </div>
+                        <div>
+                            <img className='icon' src={editIcon} alt="" />
+                            <img className='icon' src={binIcon} alt="" />
+                        </div>
+                    </div>
+                    <div className='content-fields-container'>
+                        <div className='content-entries-fields' style={{ marginLeft: '10px' }}>
+                            <span>1</span>
+                            <span>John Doe</span>
+                            <span>www.johndoe.com</span>
+                            <span>Text</span>
+                        </div>
+                        <div>
+                            <img className='icon' src={editIcon} alt="" />
+                            <img className='icon' src={binIcon} alt="" />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
---
+++ src/components/NewEntryModal/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './NewEntryModal.css';
+
+export default function NewEntryModal(props) {
+    if (!props.show) {
+        return null;
+    }
+    return (
+        <div className="modal" onClick={props.onClose}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className='modal-header'>
+                    <span className='modal-title'>New Company Profile</span>
+                </div>
+                <div className='modal-body'>
+                    <div>
+                        <span>Name</span>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <span>website</span>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <span>contact</span>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <span>Input</span>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <span>Input</span>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <span>Input</span>
+                        <input type="text" />
+                    </div>
+                </div>
+                <div className='modal-footer'>
+                    <button onClick={props.onClose} className='modal-close-button'>Close</button>
+                    <button className='modal-Add-button'>Add</button>
+                </div>
+            </div>
+        </div>
+    );
+}
